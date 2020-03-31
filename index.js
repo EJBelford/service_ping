2535eef838d947601e3f94ac4f2c49b1980234d2
VBMS-Dev-Ops <56975911+VBMS-Dev-Ops@users.noreply.github.com>
2020-03-31 14:02:30 -0400

    Initial upload of service_ping.

---
+++ index.js
@@ -0,0 +1,18 @@
+var rect = {
+    perimeter: (x,y) => (2*(x+y)), 
+    area: (x,y) =>  (x*y)
+};
+    
+function solveRect(l,w) {
+    console.log("Solving for rectangle with i = " + l + "and w = " + w);
+    
+    if (l <= 0 || w <= 0) {
+        console.log("");
+    } 
+    else {
+        console.log("The arra of the rectangle is " + rect.area(l,w));
+        console.log("The perimeter of the rectangle is " + rect.perimeter(l,w));
+    }
+};    
+
+sovleRect(2,4);
